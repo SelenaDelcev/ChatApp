7d6a7765a1e9ed1b96728aeb264de1167a3ff5de
SelenaDelcev <selenadelcev411@gmail.com>
2024-07-24 12:11:14 +0200

    Added localhost:3000 to allowed CORS origins

--- main.py
+++ main.py
@@ -26,7 +26,10 @@ logger = logging.getLogger(__name__)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nice-forest-0cdf73d0f.5.azurestaticapps.net"],
+    allow_origins=[
+        "https://nice-forest-0cdf73d0f.5.azurestaticapps.net",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
